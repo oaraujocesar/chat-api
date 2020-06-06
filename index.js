154a0826fca6c8e378869ced482f8455cdf7250d
oaraujocesar <oaraujocesar@gmail.com>
2020-06-06 17:58:20 -0300

    Enabling cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const cors = require('cors');
 
+app.use(cors());
 io.origins('*:*')
 
 io.on("connection", (socket) => {
